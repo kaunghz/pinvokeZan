6e630d8956ba0ca7632c84d80d73248737e14a24
Kaung Zan <khz24@drexel.edu>
2024-01-04 22:59:11 -0500

    first test

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#ifdef _WIN32
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT __attribute__((visibility("default")))
+#endif
+
+extern "C" DLL_EXPORT void hello();
+
+void hello() {
+    std::cout << "greetings from C++ vsc\n";
+}
